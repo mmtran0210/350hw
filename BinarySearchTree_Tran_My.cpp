674206ce4ca18ad32ac0649a15cb49775a986c09
mmtran0210 <mi.minh.tran@gmail.com>
2023-10-18 16:42:52 -0400

    pipi

--- BinarySearchTree_Tran_My.cpp
+++ BinarySearchTree_Tran_My.cpp
@@ -18,3 +18,33 @@ bstNode* getNewNode(int data) {
     newNode->left = newNode->right = NULL;
     return newNode;
 }
+
+bstNode* Insert(bstNode* root, int data){
+    // tree is empty
+    if(root==NULL){
+        root = getNewNode(data);
+    }
+    
+    // if data is less than existing node, insert to left subtree
+    else if(data <= root->data){
+        root->left = Insert(root->left, data);
+        printf("Node %d has been inserted", data);
+    }
+    // if data is greater than existing node, insert to right subtree
+    else if{
+        root->right = Insert(root->right, data);
+        printf("Node %d has been inserted", data);
+    }
+    return root;
+}
+
+bstNode* Search(bstNode* root, int data){
+
+}
+
+void Inorder(bstNode* root){
+    if(root == NULL) return;
+    Inorder(root->left);
+    printf(root->data, ", ")
+    Inorder(root->right);
+}
