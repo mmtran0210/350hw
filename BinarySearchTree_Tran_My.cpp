5434391c0612e45896afd0b4d9e75d259224f113
mmtran0210 <mi.minh.tran@gmail.com>
2023-10-25 16:42:32 -0400

    pushh

--- BinarySearchTree_Tran_My.cpp
+++ BinarySearchTree_Tran_My.cpp
@@ -39,12 +39,88 @@ bstNode* Insert(bstNode* root, int data){
 }
 
 bstNode* Search(bstNode* root, int data){
+        if (root == nullptr || root->data == data) {
+            if (root == nullptr)
+                std::cout << "Search key not found" << std::endl;
+            return root;
+        }
+        if (data < root->data) {
+            std::cout << root->data << " → ";
+            return Search(root->left, data);
+        }
+        std::cout << root->data << " → ";
+        return Search(root->right, data);
 
 }
 
-void Inorder(bstNode* root){
+void InOrder(bstNode* root){
     if(root == NULL) return;
-    Inorder(root->left);
+    InOrder(root->left);
     printf(root->data, ", ")
     Inorder(root->right);
 }
+
+  bstNode* Remove(bstNode* root, int data) {
+        if (root == nullptr) {
+            return root;
+        }
+        
+        else
+        if (data < root->data) {
+            root->left = Remove(root->left, data);
+        
+        } else if (data > root->data) {
+            root->right = Remove(root->right, data);
+
+        } else {
+            if (root->left == nullptr) {
+                Node* temp = root->right;
+                delete root;
+                return temp;
+
+            } else if (root->right == nullptr) {
+                Node* temp = root->left;
+                delete root;
+                return temp;
+            }
+
+            bstNode* temp = findMin(root->right);
+            root->data = temp->data;
+            root->right = Remove(root->right, temp->data);
+        }
+        return root;
+    }
+        bstNode* findMin(bstNode* root) {
+        while (root->left != nullptr) {
+            root = root->left;
+        }
+        return root;
+    }
+
+
+        void findMin() {
+        if (root == nullptr) {
+            std::cout << "BST is empty" << std::endl;
+        } else {
+            Node* minNode = findMin(root);
+            std::cout << "BST node with the smallest value: " << minNode->data << std::endl;
+            std::cout << "BST nodes are: ";
+            InOrder(root);
+            std::cout << std::endl;
+        }
+    }
+
+    void findMax() {
+        bstNode* current = root;
+        if (current == nullptr) {
+            std::cout << "BST is empty" << std::endl;
+        } else {
+            while (current->right != nullptr) {
+                current = current->right;
+            }
+            std::cout << "BST node with the largest value: " << current->data << std::endl;
+            std::cout << "BST nodes are: ";
+            InOrder(root);
+            std::cout << std::endl;
+        }
+    }
